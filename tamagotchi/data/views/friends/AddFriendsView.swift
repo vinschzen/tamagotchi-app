0f217d9afbcc520305c89727866408ec0bf7591e
SamuelAndrey <samuelandrey2021@gmail.com>
2024-09-18 10:00:16 +0700

    add: add friend views

---
+++ tamagotchi/data/views/friends/AddFriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  AddFriendsView.swift
+//  tamagotchi
+//
+//  Created by MacBook Pro on 18/09/24.
+//
+
+import SwiftUI
+
+struct AddFriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFriendsView()
+}
---
+++ tamagotchi/data/views/friends/MeetFriendsView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetFriendsView.swift
+//  tamagotchi
+//
+//  Created by MacBook Pro on 18/09/24.
+//
+
+import SwiftUI
+
+struct MeetFriendsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetFriendsView()
+}
