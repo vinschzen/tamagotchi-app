81531a2c2610c3fe8a3d55c4d80b2ac3897bc50c
stevanushia <stevanushiaa@gmail.com>
2024-09-23 18:16:53 +0700

    uhm

---
+++ tamagotchi/data/views/profiling/ChooseISView.swift
@@ -0,0 +1,18 @@
+//
+//  ChooseISView.swift
+//  tamagotchi
+//
+//  Created by MacBook Air on 23/09/24.
+//
+
+import SwiftUI
+
+struct ChooseISView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChooseISView()
+}
--- tamagotchi/data/views/profiling/chooseTest.swift
+++ tamagotchi/data/views/profiling/chooseTest.swift
@@ -1,18 +1,18 @@
 //
-//  Step3View.swift
+//  chooseTest.swift
 //  tamagotchi
 //
-//  Created by MacBook Air on 19/09/24.
+//  Created by MacBook Air on 23/09/24.
 //
 
 import SwiftUI
 
-struct Step3View: View {
+struct chooseTest: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    Step3View()
+    chooseTest()
 }
